dc7f745f936f78007bf891133b8ecfcb779b1817
01060106 <107b01215@mailst.cjcu.edu.tw>
2020-11-10 15:12:31 +0800

    11.10

--- templates_c/f1df6df367b62f03f69eed0258e303321d48e9bb_0.file.side_login.html.php
+++ templates_c/f1df6df367b62f03f69eed0258e303321d48e9bb_0.file.side_login.html.php
@@ -1,12 +1,41 @@
+<?php
+/* Smarty version 3.1.29, created on 2020-11-10 06:56:28
+  from "C:\UniServerZ\www\mini_shop\templates\side_login.html" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_5faa399c391197_93395707',
+  'file_dependency' => 
+  array (
+    'f1df6df367b62f03f69eed0258e303321d48e9bb' => 
+    array (
+      0 => 'C:\\UniServerZ\\www\\mini_shop\\templates\\side_login.html',
+      1 => 1604991383,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5faa399c391197_93395707 ($_smarty_tpl) {
+?>
 <html lang="en">
 <head>
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <?php echo '<script'; ?>
+ src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"><?php echo '</script'; ?>
+>
+  <?php echo '<script'; ?>
+ src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"><?php echo '</script'; ?>
+>
+  <?php echo '<script'; ?>
+ src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"><?php echo '</script'; ?>
+>
 </head>
 
 
@@ -38,4 +67,5 @@
       <button type="submit" name="button" class="btn btn-outline-success">登入</button>
     </div>
   </div>
-</form>
+</form><?php }
+}
